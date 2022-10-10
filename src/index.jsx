006b013be3fdaab0253b0b3cb8829766abdd0fab
Boris Muzalevskii <bmuza57@gmail.com>
2022-10-10 15:00:32 +0300

    Remove redux files

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 
 import store from 'store';
 
@@ -7,9 +7,4 @@ import App from './app';
 
 import './globalStyles';
 
-ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
