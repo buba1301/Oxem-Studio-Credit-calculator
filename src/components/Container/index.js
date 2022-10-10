96e11c85e7832b15dfe763333d1e45519119904a
Boris Muzalevskii <bmuza57@gmail.com>
2022-10-10 20:12:18 +0300

    Add flex containers

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,14 +2,38 @@ import React from 'react';
 
 import s from './Container.module.scss';
 
+const inputsLabels = [
+  'Желаемая сумма кредита',
+  'Первоначальный взнос',
+  'Срок лизинга',
+];
+
+const numberAndButtonsLabels = [
+  'Сумма договора лизинга',
+  'Первоначальный взнос',
+  'Оставить заявку',
+];
+
 const Container = () => {
   return (
     <main className={s.container}>
       <div className={s.headerWrap}>
-        <h1>Расчитайте стоимость автомобиля в лизинг</h1>
+        <p>Расчитайте стоимость автомобиля в лизинг</p>
       </div>
-      <div className={s.inputContainer}></div>
-      <div className={s.numbersAndBtnContainer}></div>
+      <div className={s.inputContainer}>
+        {inputsLabels.map((elem, index) => (
+          <div key={index} className={s.test}>
+            <span>{elem}</span>
+          </div>
+        ))}
+      </div>
+      <div className={s.numbersAndBtnContainer}>
+        {numberAndButtonsLabels.map((elem, index) => (
+          <div key={index} className={s.test1}>
+            {elem}
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
